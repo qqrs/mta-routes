071171ceb0a58f1db5c20140a5116e54d48f676d
Russell Smith <qqrsmith@gmail.com>
2013-05-05 16:20:41 -0400

    PEP8 compliance

--- nycmta.py
+++ nycmta.py
@@ -7,32 +7,53 @@ class TransitSystem(object):
         self.routes = {}
 
     def parse_stops(self, stops):
+        """Parse data from stops.txt"""
         stop_defs = filter(lambda st: st['location_type'] == '1', stops)
         stop_defs = uniq(stop_defs, lambda sd: sd['stop_name'])
         stations = (self.build_station(sd) for sd in stop_defs)
         self.stations = list(stations)
 
+    def parse_stop_times(self, stop_times):
+        """Parse data from stop_times.txt"""
+        for s in stop_times:
+            #(h,m,sec) = map(int, s['departure_time'].split(":"))
+            assert(len(s['departure_time']) == 8)
+            assert(len(s['trip_id']) == 27)
+            assert(len(s['stop_id']) == 4)
+            print self.parse_trip_id(s['trip_id'])
+            break
+        #print (h,m,sec)
+
+    def parse_trip_id(self, trip_id):
+        """Parse a stop_id with form: A20121216WKD_000800_1..S03R"""
+        if (len(trip_id) != 27):     # TODO: more specific exception type
+            raise Exception("trip_id with invalid length: %s" % trip_id)
+        return dict(zip(("service_id", "service_day", "trip_start_time",
+                         "trip_route", "shape_id"),
+                       (trip_id[0:12], trip_id[9:12], trip_id[13:19],
+                           trip_id[20], trip_id[23:27])))
+
     #def parse_station_entrances(self, station_entrances):
-        #station_defs = uniq(station_entrances, 
+        #station_defs = uniq(station_entrances,
                             #lambda x: (x['Line'], x['Station_Name']))
         #print len(list(station_defs))
         ##stations = (self.build_station(d) for d in station_defs)
         ##self.stations = list(stations)
 
     def build_station(self, st_def):
-        st = Station(st_def['stop_id'], st_def['stop_name'], 
+        st = Station(st_def['stop_id'], st_def['stop_name'],
                      st_def['stop_lat'], st_def['stop_lon'])
         return st
 
     #def build_station(self, station_def):
         #route_list = list()
         #st = Station(
-                #station_def['Line'], 
-                #station_def['Station_Name'], 
-                #station_def['Latitude'], 
-                #station_def['Longitude'], 
+                #station_def['Line'],
+                #station_def['Station_Name'],
+                #station_def['Latitude'],
+                #station_def['Longitude'],
                 #route_list)
-        #routes = [self.build_route(r) 
+        #routes = [self.build_route(r)
                     #for r in self.get_station_routes(station_def)]
         #route_list.extend(routes)
         #for r in route_list:
@@ -45,11 +66,13 @@ class TransitSystem(object):
         return self.routes[route_name]
 
     def get_station_routes(self, station_dict):
-        """Get non-empty route names for a station in range Route_1 to Route_11.
-        >>> TransitSystem().get_station_routes({'Route_1': '1',  'Route_2': '',  'Line': None})
+        """Get non-empty route names for station in range Route_1 to Route_11.
+        >>> TransitSystem().get_station_routes(
+                        >>> {'Route_1': '1',  'Route_2': '',  'Line': None})
         ['1']
         """
-        return filter(None, [station_dict.get('Route_%d' % i) for i in range(1,12)])
+        return filter(None,
+                      [station_dict.get('Route_%d' % i) for i in range(1, 12)])
 
 
 class Station(object):
@@ -61,11 +84,13 @@ class Station(object):
         self.routes = routes if routes is not None else []
 
     def __repr__(self):
-        return "Station(%s, %s, %s, %s, %s)" % (self.id, self.name, 
-                                        self.latitude, self.longitude,
-                                        "".join(str(r) for r in self.routes))
+        return ("Station(%s, %s, %s, %s, %s)" %
+                (self.id, self.name, self.latitude, self.longitude,
+                "".join(str(r) for r in self.routes)))
+
     def __str__(self):
         return self.name
+
 
 class Route(object):
     def __init__(self, name, stations=None):
@@ -76,10 +101,12 @@ class Route(object):
         return self.name
 
     def str_full(self):
-        return "Route(%s, stations=%s)" % (self.name, 
-                                ",\n".join((str(st) for st in self.stations)))
+        return ("Route(%s, stations=%s)" %
+                (self.name, ",\n".join((str(st) for st in self.stations))))
+
 
 def uniq(iterable, keyfn=None):
+    pass
     """Yields unique items in iterable using keyfn to determine uniqueness.
     >>> list(uniq([1,2,2]))
     [1, 2]
@@ -94,16 +121,21 @@ def uniq(iterable, keyfn=None):
         seen.add(key)
         yield value
 
+
 def parse_csv(csvfile):
     """Read csv file and yield rows as dict, using header row as keys"""
     csvreader = csv.reader(csvfile)
     header = csvreader.next()
-    return (dict(zip(header,st)) for st in csvreader)
+    return (dict(zip(header, st)) for st in csvreader)
+
 
 def main():
     transit = TransitSystem()
     with open('google_transit/stops.txt') as csvfile:
         transit.parse_stops(parse_csv(csvfile))
+    #with open('google_transit/stop_times.txt') as csvfile:
+        #transit.parse_stop_times(parse_csv(csvfile))
+
     #with open('StationEntrances.csv', 'rb') as csvfile:
         #transit.parse_station_entrances(parse_csv(csvfile))
 
@@ -122,4 +154,4 @@ def main():
 
 
 if __name__ == "__main__":
-    ret=main()
+    ret = main()
